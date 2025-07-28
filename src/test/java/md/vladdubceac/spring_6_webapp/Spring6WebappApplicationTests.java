dce9c914d93ba3bdc9808def68f2789a2bd11ad4
Vlad <vladimir.dubceac@snt.md>
2025-07-28 21:11:28 +0300

    First commit

---
+++ src/test/java/md/vladdubceac/spring_6_webapp/Spring6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package md.vladdubceac.spring_6_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
