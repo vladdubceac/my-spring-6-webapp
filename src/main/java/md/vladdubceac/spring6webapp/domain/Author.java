8add45557fec1f94b147a1c9a93188eeb55c21f7
Vlad <vladimir.dubceac@snt.md>
2025-08-06 21:23:57 +0300

    Added 'toString()', 'hashCode()', 'equals()'

--- src/main/java/md/vladdubceac/spring6webapp/domain/Author.java
+++ src/main/java/md/vladdubceac/spring6webapp/domain/Author.java
@@ -2,6 +2,7 @@ package md.vladdubceac.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -46,5 +47,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Author author)) return false;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
